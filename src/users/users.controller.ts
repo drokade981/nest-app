938f243ec493f1c63fb9c9a37b8c7289d066a1ac
Devendra Rokade <drokade981@gmail.com>
2025-09-10 00:09:29 +0530

    update user usinng mapped types

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,19 +1,23 @@
-import { Body, Controller, DefaultValuePipe, Get, Param, ParseIntPipe, Post, Query, ValidationPipe } from "@nestjs/common";
+import { Body, Controller, DefaultValuePipe, Get, Param, ParseIntPipe, Patch, Post, Query, ValidationPipe } from "@nestjs/common";
 import { UsersService } from "./users.service";
 import { CreateUserDto } from "./dtos/create.user.dto";
+import { UpdateUserDto } from "./dtos/update-user.dto";
+import { GetUserParamDto } from "./dtos/get-user-param.dto";
 
 // http://localhost:3000/users
 @Controller('users')
 export class UsersController {
 
+    userService: UsersService;
+
     @Get()
     getUsers(@Query('limit', new DefaultValuePipe(10), ParseIntPipe) limit:number, 
-    @Query('page', new DefaultValuePipe(1), ParseIntPipe) page:number 
+        @Query('page', new DefaultValuePipe(1), ParseIntPipe) page:number, 
+        @Param() param: GetUserParamDto
     ){
         const UserService = new UsersService();
-        console.log(limit, page);
-        
-        return UserService.getAllUsers();
+        console.log(param);
+        return this.userService.getAllUsers();
     }
 
     @Get(':id')
@@ -31,5 +35,11 @@ export class UsersController {
         console.log(user instanceof CreateUserDto);
         
         return 'new user has been created';
+    }
+
+    @Patch()
+    updateUser(@Body() user: UpdateUserDto ) {
+        console.log(user);
+        return 'User updated sussessfully';
     }
 }
