c0838563c2e806f7aa33c21d0cf3bebc04366716
Devendra Rokade <drokade981@gmail.com>
2025-09-15 17:30:23 +0530

    add circular dependency between user and auth module

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,23 +1,31 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, Inject, forwardRef } from "@nestjs/common";
+import { AuthService } from "src/auth/auth.service";
 
 @Injectable()
 export class UsersService {
-    users: {id: Number, name : String, email : String, gender: String, isMarried: boolean
+
+    constructor(@Inject(forwardRef(() => AuthService))private readonly authService: AuthService) {}
+    // forward ref for circular dependancy
+
+    users: {id: Number, name : String, email : String, gender: String, isMarried: boolean, password: String
     }[] = [
-        {id:1, name: 'john', email:'john@gmail.com', gender:'male', isMarried:false},
-        {id:2, name: 'mark', email:'mark@gmail.com', gender:'male', isMarried:false},
-        {id:3, name: 'marry', email:'marry@gmail.com', gender:'female', isMarried:true},
+        {id:1, name: 'john', email:'john@gmail.com', gender:'male', isMarried:false, password: 'Test123'},
+        {id:2, name: 'mark', email:'mark@gmail.com', gender:'male', isMarried:false, password: 'Test123'},
+        {id:3, name: 'marry', email:'marry@gmail.com', gender:'female', isMarried:true, password: 'Test123'},
     ];
 
     getAllUsers() {
-        return this.users;
+        if(this.authService.isAuthenticated) {
+            return this.users;
+        }
+        return 'You are not logged in';
     }
 
     getUserById(id: Number){
         return this.users.find(x => x.id === id);
     }
 
-    createUser(user: {id: Number, name: String, email: String, gender: string, isMarried: boolean}) {
+    createUser(user: {id: Number, name: String, email: String, gender: string, isMarried: boolean, password: String}) {
         this.users.push(user);
     }
 }
--- update.md
+++ update.md
@@ -13,6 +13,7 @@ it will create a tweet folder inside "src" folder with tweet.module.ts file and 
 $ nest g controller tweet
 ```
 
+add --no-spec  for no spec file
 Create user service and use it in controller
 
 ## create DTO
@@ -39,3 +40,8 @@ it will create tweet.service.ts in src/tweet folder and link it with controller 
 
 inter dependency injection
 we have to export the service in same module and inport the module not service in another module and inject it in constructor method of service and use it's function in service or in controller
+
+-- circular dependency 
+use forward ref for circular dependancy in module and inject in service construct method
+
+constructor(@Inject(forwardRef(() => AuthService))private readonly authService: AuthService) {}
