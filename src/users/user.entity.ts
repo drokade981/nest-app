222f2782774daac6c6ea101d01fd6a4a15f6ec86
Devendra Rokade <drokade981@gmail.com>
2025-09-23 00:32:35 +0530

    create user table

---
+++ src/users/user.entity.ts
@@ -0,0 +1,40 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class User {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({
+        type: 'varchar', // check if the db is support varchar
+        nullable: false,
+        length: 100
+    })
+    firstName: string;
+
+    @Column({
+        type: 'varchar',
+        nullable: false,
+        length: 100
+    })
+    lastName: string;
+
+    @Column({
+        type: 'varchar',
+        nullable: true,
+        length: 10
+    })
+    gender: string;
+
+    @Column({
+        type: 'varchar',
+        nullable: false,
+        length: 100,
+        unique: true
+    })
+    email: string;
+
+    @Column()
+    password: string;
+
+}
